507a99f4fde3d9886690885857852159cc5b25cd
Nyarsh <sharynmay4@gmail.com>
2026-05-28 08:24:08 +0800

    3306 na

--- tutor/tutor_session_request.php
+++ tutor/tutor_session_request.php
@@ -11,7 +11,7 @@ $username = "root";
 $password = "";
 $dbname = "tutorloop_db";
 
-$conn = new mysqli($servername, $username, $password, $dbname, 3307);
+$conn = new mysqli($servername, $username, $password, $dbname, 3306);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
